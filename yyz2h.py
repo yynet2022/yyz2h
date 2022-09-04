b5f90483cd4860ad4a32371338e9227c2989a2ca
Yokota Yoshinori <yokota@yynet.org>
2022-09-04 09:42:30 +0900

    登録

---
+++ yyz2h.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+import sys
+import os
+import pathlib
+import tempfile
+
+ZN = ''.join(chr(0xff01 + i) for i in range(94)) + '　'
+HN = ''.join(chr(0x21 + i) for i in range(94)) + ' '
+Z2H = str.maketrans(ZN, HN)
+# print(s.translate(Z2H))
+
+for f in sys.argv[1:]:
+    try:
+        with open(f, 'rb') as rfd:
+            b = rfd.read()
+            try:
+                ln = b.decode('utf-8')
+            except UnicodeDecodeError:
+                ln = b.decode('sjis')
+
+            p = pathlib.PurePath(f)
+            (wfd, n) = tempfile.mkstemp(
+                suffix='_' + p.name, prefix='z2h_', dir=p.parent)
+
+            os.write(wfd, ln.translate(Z2H).encode('utf-8'))
+            os.close(wfd)
+    except Exception as e:
+        print(str(e))
